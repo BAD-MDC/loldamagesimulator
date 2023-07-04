6a002c034b18f383bc69dbc3c9441bd8756aaf87
dongkam <84716824+dongkam5@users.noreply.github.com>
2023-07-04 20:24:15 +0900

    change App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React222
         </a>
       </header>
     </div>
