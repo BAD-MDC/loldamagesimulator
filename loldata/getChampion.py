1d8bf56682904349f9e8d37a632e12e852103467
crAy <craycoding@gmail.com>
2023-07-14 12:44:01 +0900

    champion info readme

--- loldata/getChampion.py
+++ loldata/getChampion.py
@@ -30,6 +30,26 @@ def writeChampionJSON(champion_name) :
     with open(champion_json_file_name, 'w') as convert_file:
         json.dump(champion(champion_name), convert_file, ensure_ascii = False)
 
+def writeChampionTXT(champion_name) :
+    champion_txt_file_name = open('./champion_info/' + champion_name + '.txt', 'w')
+    champion_txt_file_name.write(str(champion(champion_name)['data'][champion_name]['stats']['hp']) + '\n')
+    champion_txt_file_name.write(str(champion(champion_name)['data'][champion_name]['stats']['hpperlevel']) + '\n')
+    champion_txt_file_name.write(str(champion(champion_name)['data'][champion_name]['stats']['mp']) + '\n')
+    champion_txt_file_name.write(str(champion(champion_name)['data'][champion_name]['stats']['mpperlevel']) + '\n')
+    champion_txt_file_name.write(str(champion(champion_name)['data'][champion_name]['stats']['armor']) + '\n')
+    champion_txt_file_name.write(str(champion(champion_name)['data'][champion_name]['stats']['armorperlevel']) + '\n')
+    champion_txt_file_name.write(str(champion(champion_name)['data'][champion_name]['stats']['spellblock']) + '\n')
+    champion_txt_file_name.write(str(champion(champion_name)['data'][champion_name]['stats']['spellblockperlevel']) + '\n')
+    champion_txt_file_name.write(str(champion(champion_name)['data'][champion_name]['stats']['attackdamage']) + '\n')
+    champion_txt_file_name.write(str(champion(champion_name)['data'][champion_name]['stats']['attackdamageperlevel']) + '\n')
+    champion_txt_file_name.write(str(champion(champion_name)['data'][champion_name]['stats']['attackspeed']) + '\n')
+    champion_txt_file_name.write(str(champion(champion_name)['data'][champion_name]['stats']['attackspeedperlevel']) + '\n')
 
-#for champ in champions()['data'] :
+
+
+for champ in champions()['data'] :
+    writeChampionTXT(champ)
+#    print(champ)
 #    writeChampionJSON(champ)
+
+#print(champion('Aatrox'))
